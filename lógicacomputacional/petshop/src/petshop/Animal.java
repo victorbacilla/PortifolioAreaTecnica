8493eee0d52cd28f5e4a07c2273ac41b0d87c180
Victor Bacilla de Jesus <82882285+victorbacilla@users.noreply.github.com>
2021-11-19 09:13:49 -0300

    Add files via upload
    add

---
+++ lógicacomputacional/petshop/src/petshop/Animal.java
@@ -0,0 +1,21 @@
+package petshop;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SESI_SENAI
+ */
+public class Animal {
+    int matricula;
+    String nome;
+    String cor_pelagem;
+    String genero;
+    int idade;
+    float peso;
+    
+}
