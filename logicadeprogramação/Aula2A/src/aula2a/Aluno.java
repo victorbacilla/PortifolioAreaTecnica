6ba64fd8bcc1202fc446fcd6715d35119e6ed816
Victor Bacilla de Jesus <82882285+victorbacilla@users.noreply.github.com>
2021-11-19 09:10:06 -0300

    Add files via upload
    add

---
+++ logicadeprogramação/Aula2A/src/aula2a/Aluno.java
@@ -0,0 +1,20 @@
+
+package aula2a;
+
+
+public class Aluno extends Pessoa{
+    String matricula;
+
+    public Aluno(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }    
+    
+}
---
+++ logicadeprogramação/Aula2A/src/aula2a/Pessoa.java
@@ -0,0 +1,33 @@
+package aula2a;
+
+public class Pessoa {
+    String nome;
+    String cpf;
+    int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+}
